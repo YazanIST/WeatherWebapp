29a0e85baf55f04c23778e6ad421d513c38007f8
YazanIST <yazan.nizam2@gmail.com>
2022-10-04 14:04:21 +0300

    updated the cleaner script

--- cleaner.ps1
+++ cleaner.ps1
@@ -1,7 +1,7 @@
 docker stop (docker ps -aq)
 docker rm (docker ps -aq)
-docker rmi containerizationproject-analyticsservice
-docker rmi containerizationproject-entryapp
-docker rmi containerizationproject-authenticationservice
-docker rmi containerizationproject-analytics_service
-docker rmi containerizationproject-show_results
+docker rmi weatherwebapp-analyticsservice
+docker rmi weatherwebapp-entryapp
+docker rmi weatherwebapp-authenticationservice
+docker rmi weatherwebapp-analytics_service
+docker rmi weatherwebapp-show_results
